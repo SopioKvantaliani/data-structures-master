3e036c9d1118c599bd997e8be4cf845d3eb288f4
sopiokvantaliani <skvantaliani@gmail.com>
2023-05-03 07:39:11 -0400

    Queue DataType

---
+++ dsa-07-Queues/src/main/java/Review6QueuesAndHashing/KeyValue.java
@@ -0,0 +1,12 @@
+package Review6QueuesAndHashing;
+
+public class KeyValue <U, V> {
+
+    U key;
+    V value;
+
+    public KeyValue(U key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
